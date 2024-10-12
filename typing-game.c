0b852b79e7495518864f65271fee847652799ff0
kevinmccall <kevinmccalldev@gmail.com>
2024-10-12 17:21:47 -0400

    fixed calculations of wpm and additional output info

--- typing-game.c
+++ typing-game.c
@@ -28,7 +28,7 @@
 
 FILE *fptr;
 char file_line[LINE_LENGTH], display_line[DISPLAY_LENGTH], *file_name;
-int count, num_skipped, is_correct, num_correct, num_chars, num_correct_adjusted, num_chars_adjusted, correct[LINE_LENGTH];
+int count, num_skipped, was_correct, num_correct_adjusted, num_chars_adjusted, correct[LINE_LENGTH];
 size_t idx;
 time_t start_time, end_time;
 
@@ -50,7 +50,7 @@ void do_display_line(char *file_line, char *display_line) {
     int newline_index = strcspn(display_line, "\n"); 
     strncpy(display_line + newline_index, "\\n\n", 4);
     printf(CLEAR RESET "%s", display_line);
-    if (is_correct) {
+    if (was_correct) {
         printf(GREEN);
     } else {
         printf(RED);
@@ -60,13 +60,15 @@ void do_display_line(char *file_line, char *display_line) {
 void display_stats() {
     printf(RESET CLEAR);
     end_time = time(NULL);
-    float accuracy = 100.0 * num_correct / num_chars;
-    float time_taken = (end_time - start_time) / 60.0;
+    float accuracy = 100.0 * num_correct_adjusted / num_chars_adjusted;
+    float time_taken_minutes = (end_time - start_time) / 60.0;
     int minutes_taken = (end_time - start_time) / 60;
     int seconds_taken = (end_time - start_time) % 60;
-    float gross_wpm = num_chars / 5.0 / time_taken;
-    float net_wpm = (num_chars / 5 - (num_chars_adjusted - num_correct_adjusted)) / time_taken;
+    float gross_wpm = num_chars_adjusted / 5.0 / time_taken_minutes;
+    float net_wpm = num_correct_adjusted / 5.0 / time_taken_minutes;
 
+    printf("%s\n", file_name);
+    printf("Characters Typed: %d\n", num_chars_adjusted);
     printf("Accuracy: %.2f%%\n", accuracy);
     printf("Time Taken: %dm %ds\n", minutes_taken, seconds_taken);
     printf("WPM Gross: %.0f\n", gross_wpm);
@@ -97,7 +99,7 @@ void init(int argc, char **argv) {
         exit(1);
     }
     start_time = time(NULL);
-    count = num_skipped = idx = is_correct = num_chars_adjusted = num_correct_adjusted = num_correct = num_chars = 0;
+    count = num_skipped = idx = was_correct = num_chars_adjusted = num_correct_adjusted = 0;
 
     if (init_input()) error_exit("cannot initialize input");
     signal(SIGTERM, ctrlc_handler);
@@ -124,6 +126,11 @@ int main(int argc, char **argv) {
             if (player_in == '\b') {
                 if (idx > 0) {
                     idx--;
+                    num_chars_adjusted--;
+                    if (correct[idx]) {
+                        num_correct_adjusted--;
+                        correct[idx] = 0;
+                    }
                     putchar('\b');
                 }
                 continue;
@@ -132,10 +139,12 @@ int main(int argc, char **argv) {
             } else if (player_in == '\t') {
                 while (idx < line_length && !is_whitespace(file_line[idx])) {
                     idx += 1;
+                    num_skipped += 1;
                     putchar(' ');
                 }
                 while (idx < line_length && is_whitespace(file_line[idx])) {
                     idx += 1;
+                    num_skipped += 1;
                     putchar(' ');
                 }
                 continue;
@@ -144,26 +153,22 @@ int main(int argc, char **argv) {
             // update correctness
             correct[idx] = file_line[idx] == player_in;
             if (correct[idx]) {
-                if (!is_correct) {
+                if (!was_correct) {
                     printf(GREEN);
                 }
-                is_correct = 1;
-                num_correct++;
+                was_correct = 1;
+                num_correct_adjusted++;
             } else {
-                if (is_correct) {
+                if (was_correct) {
                     printf(RED);
                 }
-                is_correct = 0;
+                was_correct = 0;
             }
-            num_chars++;
+            num_chars_adjusted++;
 
             // display player input
             putchar(player_in);
             ++idx;
-        }
-        num_chars_adjusted += idx;
-        for (int i = 0; i < idx; i++) {
-            if (correct[i]) num_correct_adjusted++;
         }
         idx = 0;
     }
